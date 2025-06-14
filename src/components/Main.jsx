e379d477e0b3d2c373d01d28c3a1d5b5a507b7e4
KRISH <addresskrish@gmail.com>
2025-06-15 02:49:43 +0530

    main started

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+function Main() {
+    return (
+        <>
+            <main>
+                <div className="main-main">
+                    <div className="main-1">
+                        <img height="250px" src="https://jumpg-assets.tokyo-cdn.com/secure/title/100277/title_thumbnail_portrait_list/315202.jpg?hash=VyvSe_DKNkyjLbIVoRd_qQ&expires=2145884400" alt="manga-img" />
+                        <h2>Rugby Rumble</h2>
+                    </div>
+                    <div className="main-2">
+                        <h3>#001</h3>
+                        <p>Chapter 01: Fight with Them</p>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default Main
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,39 @@
+import mainLogo from "../assets/main-logo.png"
+
 function Navbar() {
+
+    function showSidebar() {
+        const sidebar = document.querySelector('.nav-ul-sidebar');
+        sidebar.style.display = 'flex';
+    }
+
+    function closeSidebar() {
+        const closeSidebar = document.querySelector('.nav-ul-sidebar')
+        closeSidebar.style.display = 'none';
+    }
+
     return (
         <>
             <nav>
                 <div className="nav-p1">
-                    <img src="https://www.pngall.com/wp-content/uploads/13/Anime-Logo-PNG-HD-Image.png" alt="main-logo" height="100px"/>
+                    <ul className="nav-ul-sidebar">
+                        <li onClick={closeSidebar} className="nav-li"><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e3e3e3"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></li>
+                        <li className="nav-li"><a href="#">Home</a></li>
+                        <li className="nav-li"><a href="#">About</a></li>
+                        <li className="nav-li"><a href="#">Contact</a></li>
+                    </ul>
+                    
+                    <img src={mainLogo} alt="main-logo" height="100px"/>
                     <ul className="nav-ul">
-                        <li className="nav-li">Home</li>
-                        <li className="nav-li">About</li>
-                        <li className="nav-li">Contact</li>
+                        <li id="hide" className="nav-li"><a href="#">Home</a></li>
+                        <li id="hide" className="nav-li"><a href="#">About</a></li>
+                        <li id="hide" className="nav-li"><a href="#">Contact</a></li>
                     </ul>
                 </div>
                 <div className="nav-p2">
-                    <button className="star">Github</button>
-                    <button className="login">Login</button>
+                    <a id="hide" href="#">Github</a>
+                    <a id="hide" href="#">Login</a>
+                    <a href="#" onClick={ showSidebar } ><svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e3e3e3"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a>
                 </div>
             </nav>
         </>
